aa4cf6da3dae03e35b822efb34fe4891813592ed
Degner3 <niklaschristiansen@live.dk>
2023-03-29 16:26:00 +0200

    S

--- Assets/JS/test.js
+++ Assets/JS/test.js
@@ -1,5 +1,5 @@
 
-
+// Test
 if ("geolocation" in navigator) {
     console.log('geolocation available');
     navigator.geolocation.getCurrentPosition((position) => {
@@ -11,9 +11,45 @@ if ("geolocation" in navigator) {
         // console.log(position.coords.latitude);
         // console.log(position.coords.longitude);
 });
-  } else {
+} else {
     console.log('geolocation not available');
+}
+
+
+
+
+
+// Y Test
+const myState = () => {
+
+  const status = document.querySelector('.status');
+
+  const success = (position) => {
+    console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude + ' ' + longitude);
+
+    const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+
+
+    fetch(geoApiUrl)
+
+    .then(response => response.json())
+
+    .then(data => {
+      console.log(data);
+      status.textContent = data.principalSubdivision
+    })
+
   }
 
+  const error  = () => {
+    status.textContent = 'Unable to retrieve your location';
+  }
 
+  navigator.geolocation.getCurrentPosition(success, error);
 
+}
+
+document.querySelector('.find-state').addEventListener('click', myState);
